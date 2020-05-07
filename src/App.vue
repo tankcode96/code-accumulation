ffbba36073f59a597fc2a8c153905f5b2ad524cb
tankm <tan_km@163.com>
2020-05-07 23:54:19 +0800

    更新防抖代码

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,7 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '@/styles/style.scss';
 
 #nav {
   padding: 30px;
--- src/components/debounce/Debounce.vue
+++ src/components/debounce/Debounce.vue
@@ -1,11 +1,47 @@
 <template>
-  <div class="comp debounce-comp"></div>
+  <div class="comp debounce-comp">
+    <div>
+      <span>次数：{{ normalTimes }}</span>
+      <input type="button" value="点击-非防抖" @click="handleNormalClick" />
+    </div>
+    <div>
+      <span>次数：{{ debounceTimes }}</span>
+      <input type="button" value="点击-防抖" @click="handleDebounceClick" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component({
   name: "debounce"
-};
+})
+class DebounceComp extends Vue {
+  normalTimes = 0; // 正常情况下的次数
+  debounceTimes = 0; // 防抖情况下的次数
+
+  /**
+   * 防抖方法
+   */
+  debounce() {}
+
+  /**
+   * 处理页面事件：点击按钮-非防抖
+   */
+  handleNormalClick() {
+    this.normalTimes++;
+  }
+
+  /**
+   * 处理页面事件：点击按钮-非防抖
+   */
+  handleDebounceClick() {
+    this.debounceTimes++;
+  }
+}
+export default DebounceComp;
 </script>
 
 <style lang="scss" scoped></style>
