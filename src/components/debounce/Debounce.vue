e362732a67a0748da03e22ca524c7fdf2871c131
kunming100 <kim_tankunming@outlook.com>
2020-08-24 15:10:23 +0800

    更新项目依赖，整理throttle、debounce的代码

--- src/components/debounce/Debounce.vue
+++ src/components/debounce/Debounce.vue
@@ -19,133 +19,119 @@
 </template>
 
 <script>
-import { Vue, Component, Watch } from "vue-property-decorator";
+  import { Vue, Component, Watch } from 'vue-property-decorator'
 
-@Component({
-  name: "debounce"
-})
-class DebounceComp extends Vue {
-  @Watch("currentFocus")
-  currentFocusWatcher(newVal, oldVal) {
-    if (newVal !== oldVal) {
-      this.consoleList = [];
+  @Component({
+    name: 'debounce'
+  })
+  class DebounceComp extends Vue {
+    @Watch('currentFocus')
+    currentFocusWatcher(newVal, oldVal) {
+      if (newVal !== oldVal) {
+        this.consoleList = []
+      }
+    }
+
+    currentFocus = '' // 当前聚焦的输入框 without:非防抖 with:防抖
+    consoleList = [] // 输出到页面的内容列表
+    debounceTimer = null // 防抖方法-定时器对象
+
+    /**
+     * 函数防抖
+     * @param func 函数
+     * @param wait 延迟执行毫秒数
+     * @param immediate true 表立即执行，false 表非立即执行
+     */
+    debounce(func, wait, immediate) {
+      let timeout
+
+      return function() {
+        let context = this
+        let args = arguments
+
+        if (timeout) clearTimeout(timeout)
+        if (immediate) {
+          var callNow = !timeout
+          timeout = setTimeout(() => {
+            timeout = null
+          }, wait)
+          if (callNow) func.apply(context, args)
+        } else {
+          timeout = setTimeout(function() {
+            func.apply(context, args)
+          }, wait)
+        }
+      }
+    }
+
+    /**
+     * 处理页面事件：输入框聚焦
+     */
+    handleFocus(type) {
+      try {
+        this.currentFocus = type
+        this.debounce()
+          .then(() => {
+            console.log('resolve')
+          })
+          .catch(e => {
+            console.log('reject', e)
+          })
+      } catch (e) {
+        console.log('catch')
+      }
+    }
+
+    /**
+     * 处理页面事件：输入-非防抖
+     */
+    handleWithoutDebounceInput(e) {
+      this.ajax(e.target.value)
+    }
+
+    /**
+     * 处理页面事件：输入-防抖
+     */
+    handleDebounceInput(e) {
+      this.ajax(e.target.value)
+    }
+
+    /**
+     * 请求方法
+     */
+    ajax(value) {
+      this.consoleList.unshift(value)
     }
   }
-
-  currentFocus = ""; // 当前聚焦的输入框 without:非防抖 with:防抖
-  consoleList = []; // 输出到页面的内容列表
-  debounceTimer = null; // 防抖方法-定时器对象
-
-  /**
-   * 防抖方法
-   */
-  debounce() {
-    return new Promise((resolve, reject) => {
-      const a = 1;
-      if (a === 1) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
-  }
-
-  /**
-   * 处理页面事件：输入框聚焦
-   */
-  handleFocus(type) {
-    try {
-      this.currentFocus = type;
-      this.debounce()
-        .then(() => {
-          console.log("resolve");
-        })
-        .catch(e => {
-          console.log("reject", e);
-        });
-    } catch (e) {
-      console.log("catch");
-    }
-  }
-
-  /**
-   * 处理页面事件：输入-非防抖
-   */
-  handleWithoutDebounceInput(e) {
-    this.ajax(e.target.value);
-  }
-
-  /**
-   * 处理页面事件：输入-防抖
-   */
-  handleDebounceInput(e) {
-    this.ajax(e.target.value);
-  }
-
-  /**
-   * 请求方法
-   */
-  ajax(value) {
-    this.consoleList.unshift(value);
-  }
-
-  /**
-   * 函数防抖
-   * @param func 函数
-   * @param wait 延迟执行毫秒数
-   * @param immediate true 表立即执行，false 表非立即执行
-   */
-  debounce(func, wait, immediate) {
-    let timeout;
-
-    return function() {
-      let context = this;
-      let args = arguments;
-
-      if (timeout) clearTimeout(timeout);
-      if (immediate) {
-        var callNow = !timeout;
-        timeout = setTimeout(() => {
-          timeout = null;
-        }, wait);
-        if (callNow) func.apply(context, args);
-      } else {
-        timeout = setTimeout(function() {
-          func.apply(context, args);
-        }, wait);
-      }
-    };
-  }
-}
-export default DebounceComp;
+  export default DebounceComp
 </script>
 
 <style lang="scss" scoped>
-.debounce-comp {
-  display: flex;
+  .debounce-comp {
+    display: flex;
 
-  .operate-area {
-    width: 400px;
-    height: 400px;
+    .operate-area {
+      width: 400px;
+      height: 400px;
 
-    .row {
-      margin-bottom: 22px;
+      .row {
+        margin-bottom: 22px;
 
-      .row-span {
-        display: inline-block;
-        width: 120px;
-        padding-right: 10px;
-        text-align: right;
+        .row-span {
+          display: inline-block;
+          width: 120px;
+          padding-right: 10px;
+          text-align: right;
+        }
       }
     }
-  }
 
-  .view-area {
-    width: 300px;
-    height: 400px;
-    overflow: auto;
-    border: 1px solid #ccc;
-    border-radius: 6px;
+    .view-area {
+      width: 300px;
+      height: 400px;
+      overflow: auto;
+      border: 1px solid #ccc;
+      border-radius: 6px;
+    }
   }
-}
 </style>
--- src/mixins/algorithm.js
+++ src/mixins/algorithm.js
@@ -1,15 +1,17 @@
-const reverseNum = (num) => {
-	const newNum = Number(num)
-	// 判断是正数还是负数
-	const isPositiveNum = newNum >= 0
-	// 转成字符串
-	const str = newNum.toString()
-	const frontStuff = isPositiveNum ? '' : str.substr(0, 1)
-	// 反转字符串
-	const reverseStr = (isPositiveNum ? str : str.substr(1)).split().reverse()
-	// 转成数字
-	const reverseNum = Number(`${frontStuff}${reverseStr.join()}`)
-	return reverseNum
+/** @format */
+
+const reverseNum = num => {
+  const newNum = Number(num)
+  // 判断是正数还是负数
+  const isPositiveNum = newNum >= 0
+  // 转成字符串
+  const str = newNum.toString()
+  const frontStuff = isPositiveNum ? '' : str.substr(0, 1)
+  // 反转字符串
+  const reverseStr = (isPositiveNum ? str : str.substr(1)).split().reverse()
+  // 转成数字
+  const reverseNum = Number(`${frontStuff}${reverseStr.join()}`)
+  return reverseNum
 }
 
 console.log(typeof reverseNum('-123'))
